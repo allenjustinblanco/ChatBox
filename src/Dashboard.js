b95621af1c9582f2baf68cb47a262512ee5219de
allenjustinblanco <blanco.allenjustin@gmail.com>
2020-04-26 00:56:27 -0500

    Add topics and chat windows

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,9 +1,79 @@
 import React from 'react'
 
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Chip from '@material-ui/core/Chip'
+
+const useStyles = makeStyles( theme => ({
+    root: {
+        margin: '50px',
+        padding: theme.spacing(3,2),
+    },
+    flex: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    chatWindow: {
+        width: '70%'
+    },
+    topicsWindow: {
+        width: '30%',
+        height: '300px',
+        borderRight: '1px solid grey'
+    },
+    chatBox: {
+        width: '85%',
+        padding: '20px'
+    },
+    button: {
+        width: '15%'
+    },
+  }));
+  
+
 export default function Dashboard() {
+    const classes = useStyles();
+
     return (
         <div>
-            <p>Dashboard</p>
+            <Paper className={classes.root}>
+                <Typography variant="h4" component="h4">
+                    ChatBox
+                </Typography>
+                <Typography variant="h5" component="h5">
+                    Sheet of Paper
+                </Typography>
+                <div className={classes.flex}>
+                    <div className={classes.topicsWindow}>
+                        <List>
+                            {
+                                ['topic'].map(topic => (
+                                    <ListItem key={topic} button>
+                                        <ListItemText primary={topic}/>
+                                    </ListItem>
+                                ))
+                            }
+                        </List>
+                    </div>
+                    <div className={classes.chatWindow}>
+                        {
+                            [{from: 'user', msg: 'hello'}].map((chat, i) => (
+                                <div className={classes.flex} key={i} >
+                                    <Chip label={chat.from} className={classes.chip} />
+                                    <Typography variant='p'>{chat.msg}</Typography>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+                <div className={classes.flex}>
+
+                </div>
+            </Paper>
         </div>
     )
 }
